6170e53170856c9914277185681bd12810394fc0
Eli Young <eliyoung4now@gmail.com>
2025-02-03 16:17:41 +0100

    small movement changes

--- Assets/Scripts/OldWizardScript.cs
+++ Assets/Scripts/OldWizardScript.cs
@@ -49,30 +49,13 @@ public class OldWizardScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Debug.Log("update");
         bool isMoving = false;
 
-        // Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         inputY = 0;
         inputX = 0;
         playerSpriteRenderer.flipX = false;
-       
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            isMoving = true;
-            frameTimerUp -= Time.deltaTime;
-            if (frameTimerUp <= 0) {
-                frameIndexUp++;
-                if (frameIndexUp >= framesUp.Length) {
-                    frameIndexUp = 0;
-                }
-                frameTimerUp = (1f / framesPerSecond);
-                playerSpriteRenderer.sprite = framesUp[frameIndexUp];
-            }
-            if (lastDirection == "left") {
-                playerSpriteRenderer.flipX = true;
-            }
-            inputY = 1;
-        }
+
+        //up
         if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow)) {
             isMoving = true;
             frameTimerUp -= Time.deltaTime;
@@ -104,29 +87,25 @@ public class OldWizardScript : MonoBehaviour
             lastDirection = "left";
             playerSpriteRenderer.flipX = true;
         }
-        if (Input.GetKey(KeyCode.LeftArrow)) {
+        else if (Input.GetKey(KeyCode.UpArrow)) {
             isMoving = true;
-            frameTimerLeft -= Time.deltaTime;
-            if (frameTimerLeft <= 0) {
-                frameIndexLeft++;
-                if (frameIndexLeft >= framesRight.Length) {
-                    frameIndexLeft = 0;
+            frameTimerUp -= Time.deltaTime;
+            if (frameTimerUp <= 0) {
+                frameIndexUp++;
+                if (frameIndexUp >= framesUp.Length) {
+                    frameIndexUp = 0;
                 }
-                frameTimerLeft = (1f / framesPerSecond);
-                playerSpriteRenderer.sprite = framesRight[frameIndexLeft];
+                frameTimerUp = (1f / framesPerSecond);
+                playerSpriteRenderer.sprite = framesUp[frameIndexUp];
             }
-            playerSpriteRenderer.flipX = true;
-            inputX = -1;
-            lastDirection = "left";
-        }
-        if (Input.GetKey(KeyCode.DownArrow) && collideCheck.IsCollided) {
-            isMoving = true;
-            playerSpriteRenderer.sprite = spriteDown;
             if (lastDirection == "left") {
                 playerSpriteRenderer.flipX = true;
             }
-            inputY = -1;
+            inputY = 1;
         }
+
+
+        //down
         if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow) && collideCheck.IsCollided) {
             isMoving = true;
             playerSpriteRenderer.sprite = spriteDown;
@@ -142,17 +121,9 @@ public class OldWizardScript : MonoBehaviour
             inputY = -1;
             inputX = -1;
         }
-        if (Input.GetKey(KeyCode.DownArrow)) {
+        if (Input.GetKey(KeyCode.DownArrow) && collideCheck.IsCollided) {
             isMoving = true;
-            frameTimerDown -= Time.deltaTime;
-            if (frameTimerDown <= 0) {
-                frameIndexDown++;
-                if (frameIndexDown >= framesDown.Length) {
-                    frameIndexDown = 0;
-                }
-                frameTimerDown = (1f / framesPerSecond);
-                playerSpriteRenderer.sprite = framesDown[frameIndexDown];
-            }
+            playerSpriteRenderer.sprite = spriteDown;
             if (lastDirection == "left") {
                 playerSpriteRenderer.flipX = true;
             }
@@ -173,7 +144,7 @@ public class OldWizardScript : MonoBehaviour
             inputY = -1;
             inputX = 1;
         }
-        if (Input.GetKey(KeyCode.LeftArrow)) {
+        if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow)) {
             isMoving = true;
             frameTimerDown -= Time.deltaTime;
             if (frameTimerDown <= 0) {
@@ -184,10 +155,46 @@ public class OldWizardScript : MonoBehaviour
                 frameTimerDown = (1f / framesPerSecond);
                 playerSpriteRenderer.sprite = framesDown[frameIndexDown];
             }
-            playerSpriteRenderer.flipX = true;
             lastDirection = "left";
+            playerSpriteRenderer.flipX = true;
+            inputY = -1;
+            inputX = -1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            isMoving = true;
+            frameTimerDown -= Time.deltaTime;
+            if (frameTimerDown <= 0) {
+                frameIndexDown++;
+                if (frameIndexDown >= framesDown.Length) {
+                    frameIndexDown = 0;
+                }
+                frameTimerDown = (1f / framesPerSecond);
+                playerSpriteRenderer.sprite = framesDown[frameIndexDown];
+            }
+            if (lastDirection == "left") {
+                playerSpriteRenderer.flipX = true;
+            }
             inputY = -1;
         }
+
+        //left
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            isMoving = true;
+            frameTimerLeft -= Time.deltaTime;
+            if (frameTimerLeft <= 0) {
+                frameIndexLeft++;
+                if (frameIndexLeft >= framesRight.Length) {
+                    frameIndexLeft = 0;
+                }
+                frameTimerLeft = (1f / framesPerSecond);
+                playerSpriteRenderer.sprite = framesRight[frameIndexLeft];
+            }
+            playerSpriteRenderer.flipX = true;
+            lastDirection = "left";
+            inputX = -1;
+        }
+
+        //right
         if (Input.GetKey(KeyCode.RightArrow)) {
             isMoving = true;
             frameTimerRight -= Time.deltaTime;
@@ -203,6 +210,8 @@ public class OldWizardScript : MonoBehaviour
             lastDirection = "right";
         }
 
+
+
         //only use static sprites if we're not moving
         if (!isMoving) {
             if (rb2d.linearVelocity.y < 0) {
@@ -214,9 +223,6 @@ public class OldWizardScript : MonoBehaviour
                     }
                     frameTimerDown = (1f / framesPerSecond);
                     playerSpriteRenderer.sprite = framesDown[frameIndexDown];
-                    // Debug.Log("Current frame index: " + frameIndexDown);
-                    // Debug.Log("Direction: " + "down");
-                    // Debug.Log("Sprite: " + playerSpriteRenderer.sprite);
                 }
                 if (lastDirection == "left") {
                     playerSpriteRenderer.flipX = true;
@@ -231,9 +237,6 @@ public class OldWizardScript : MonoBehaviour
                     }
                     frameTimerUp = (1f / framesPerSecond);
                     playerSpriteRenderer.sprite = framesUp[frameIndexUp];
-                    // Debug.Log("Current frame index: " + frameIndexDown);
-                    // Debug.Log("Direction: " + "down");
-                    // Debug.Log("Sprite: " + playerSpriteRenderer.sprite);
                 }
                 if (lastDirection == "left") {
                     playerSpriteRenderer.flipX = true;
@@ -248,9 +251,6 @@ public class OldWizardScript : MonoBehaviour
                     }
                     frameTimerUp = (1f / framesPerSecond);
                     playerSpriteRenderer.sprite = framesIdle[frameIndexIdle];
-                    // Debug.Log("Current frame index: " + frameIndexDown);
-                    // Debug.Log("Direction: " + "down");
-                    // Debug.Log("Sprite: " + playerSpriteRenderer.sprite);
                 }
                 if (lastDirection == "left") {
                     playerSpriteRenderer.flipX = true;
